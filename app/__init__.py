72ffb0884473ea00577d9415ee182d1af122ff1e
Arrishksnazzy <arishasitnikova@mail.ru>
2025-11-25 01:01:05 +0500

    Создание и "оживление интерфейса"
    Функциональный сайт, исправлены ошибки

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,25 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'main.login'
-login_manager.login_message = 'Пожалуйста, войдите в систему чтобы получить доступ к этой странице.'
-login_manager.login_message_category = 'info'
+
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    
+    # Явно указываем корневую папку
+    app = Flask(__name__,
+                template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates'),
+                static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static'))
+
+    app.config.from_object('config.Config')
+
     db.init_app(app)
     login_manager.init_app(app)
-    
-    from app import routes, models
+    login_manager.login_view = 'main.login'
+
+    from app import routes
     app.register_blueprint(routes.bp)
-    
+
     return app
